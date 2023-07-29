af91c19c4dd237b952528fcee5c82338a886c83c
pavithra <pavithraraj2113@gmail.com>
2023-07-29 02:18:46 -0700

    commit-1 done by PavithraRajrndran

---
+++ CartApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    Item=models.IntegerField()
+    product=models.CharField(max_length=30)
+    price=models.IntegerField()
+    quantity=models.IntegerField()
